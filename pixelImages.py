284dd42cb5183d2f725a6418b52544bd77a8f7be
Nickvs99 <50055073+Nickvs99@users.noreply.github.com>
2019-05-10 20:13:43 +0200

    Improved comment
    Gave the manipulations fucntion a proper name, added credentials

--- pixelImages.py
+++ pixelImages.py
@@ -1,16 +1,18 @@
+# Nick van Santen, vansantennick@gmail.com  
+
 # Be sure the images are jpg
 
 print( "Importing...")
 
 import func
 import os
-import time
 
 # Input, these files have to be in the "Images" folder
 files = []
 
-def name(path):
-    
+def manipulations(path):
+    """The manipulations made on the images"""
+
     print ("\n   Working on %s\n" %(path))
 
     # Creates a folder with the results for the current image
@@ -20,15 +22,13 @@ def name(path):
     # The variations made of the image
     func.pixelImage(path, 10, 10)
     func.animate(path)
-    # func.colorScale(path, 0)
-    # func.colorScale(path, 1)
-    # func.colorScale(path, 2)
+    func.colorScale(path, 0)
+    func.colorScale(path, 1)
+    func.colorScale(path, 2)
     func.scan(path, 280)
-    func.grey(path)
-
+    func.greyImage(path)
 
 print( "Starting...")
-
 
 if os.path.exists("Images") and len(files) == 0:
     files = os.listdir("Images")
@@ -40,17 +40,15 @@ for file in files:
     
     # Checks if "file" is a map or folder
     extension = file[len(file) - 4:len(file)]
-    if not(extension == ".jpg" or extension == ".png"):
+    if not(extension == ".jpg"):
 
         # Grabs all files in folder
         newFiles = os.listdir("Images\\%s"%(file))
 
         for file2 in newFiles:
-            name("%s\\%s"%(file, file2))
+            manipulations("%s\\%s"%(file, file2))
     else:
 
-        name(file)
-
-
+        manipulations(file)
 
 os.startfile("Results")
